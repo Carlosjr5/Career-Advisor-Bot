677a90420b0c6710aa4df71c3ff9c24c5a2c58a1
Hanzarniwin40527436 <40527436@live.napier.ac.uk>
2024-04-12 16:34:20 +0100

    Setting pipelines

--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,36 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import openai
 
-# This is a simple example for a custom action which utters "Hello World!"
+class GPT3Action(Action):
+    def name(self) -> Text:
+        return "action_gpt3_response"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        user_input = tracker.latest_message.get('text')
+
+     
+        openai.api_key = ""  #api key
+
+       
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # eingine
+            prompt=user_input,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+
+        #  generated response from the API
+        gpt3_response = response.choices[0].text.strip()
+
+        # Send the response back to the user
+        dispatcher.utter_message(text=gpt3_response)
+
+        return []
